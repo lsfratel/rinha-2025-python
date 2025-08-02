a2a70c4571c36e2a69141abb8d8e93dd7b05e5ff
Lucas Santana <lsfratel@gmail.com>
2025-08-02 15:11:32 -0300

    feat: better worker?

--- worker/__main__.py
+++ worker/__main__.py
@@ -1,102 +1,46 @@
-import os
-import signal
-import time
+import traceback
 from datetime import datetime, timezone
 
 import gevent
 import msgpack
+import redis
 import requests
-from gevent.lock import Semaphore
-from gevent.pool import Pool
 from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
 
-from backend.keydb import keydb
+from .confg import Config
 
-PROCESSORS = {
-    "default": {
-        "health": True,
-        "url": os.getenv("DEFAULT_PAYMENT_URL", "http://localhost:8001"),
-    },
-    "fallback": {
-        "health": True,
-        "url": os.getenv("FALLBAK_PAYMENT_URL", "http://localhost:8002"),
-    },
-}
-
-HEALTH_CHECK_INTERVAL = 5
-last_health_check = 0
-health_lock = Semaphore()
-pool = Pool(size=int(os.getenv("WORKER_POOL_SIZE", "250")))
-running = True
+redis = redis.StrictRedis(
+    connection_pool=redis.ConnectionPool.from_url(
+        Config.KEYDB_URL,
+        max_connections=Config.NUM_WORKERS,
+        decode_responses=False,
+    ),
+)
 
 
-def create_sessions():
-    for processor in PROCESSORS.values():
-        session = requests.Session()
-        adapter = HTTPAdapter(
-            pool_connections=int(os.getenv("HTTP_POOL_CONNECTIONS", "20")),
-            pool_maxsize=int(os.getenv("HTTP_POOL_MAXSIZE", "100")),
-            max_retries=0,
-        )
-        session.mount("http://", adapter)
-        session.mount("https://", adapter)
-        processor["session"] = session
+def get_sesstion():
+    adapter = HTTPAdapter(
+        pool_connections=Config.NUM_WORKERS,
+        pool_maxsize=Config.NUM_WORKERS * 2,
+        max_retries=Retry(
+            total=Config.NUM_OF_RETRY,
+            backoff_factor=Config.BACKOFF_FACTOR,
+            allowed_methods={"POST"},
+            status_forcelist={500},
+        ),
+    )
 
+    session = requests.Session()
+    session.mount("https://", adapter)
+    session.mount("http://", adapter)
 
-def get_health(processor: str) -> bool:
-    with health_lock:
-        return PROCESSORS[processor]["health"]
-
-
-def set_health(processor: str, health: bool):
-    with health_lock:
-        PROCESSORS[processor]["health"] = health
-
-
-def check_health(now: float = None):
-    global last_health_check
-    now = now or time.time()
-
-    if (now - last_health_check) < HEALTH_CHECK_INTERVAL:
-        return
-
-    tasks = {
-        name: gevent.spawn(
-            info["session"].get,
-            f"{info['url']}/payments/service-health",
-        )
-        for name, info in PROCESSORS.items()
-    }
-
-    gevent.joinall(tasks.values())
-
-    for name, greenlet in tasks.items():
-        try:
-            resp = greenlet.value
-            healthy = resp.status_code == 200 and not resp.json().get("failing", False)
-            set_health(name, healthy)
-        except Exception:
-            set_health(name, False)
-
-    last_health_check = now
-
-
-def get_processor():
-    check_health()
-
-    if get_health("default"):
-        return "default"
-
-    if get_health("fallback"):
-        return "fallback"
-
-    gevent.sleep(0.1)
-    return "default"
+    return session
 
 
 def store_payment(payment: dict, processor: str, timestamp: float):
     key = f"payment:{processor}:{payment['correlationId']}"
-    pipe = keydb.pipeline()
+    pipe = redis.pipeline()
 
     pipe.set(
         key,
@@ -110,66 +54,36 @@ def store_payment(payment: dict, processor: str, timestamp: float):
     pipe.execute()
 
 
-def process_payment(payment: dict):
-    processor = get_processor()
-    url = PROCESSORS[processor]["url"]
-    session = PROCESSORS[processor]["session"]
-    dt = datetime.now(timezone.utc)
+def process_payment(payment: dict, session: requests.Session):
+    requested_at = datetime.now(timezone.utc)
+    timestamp = requested_at.timestamp()
 
-    try:
-        resp = session.post(
-            f"{url}/payments",
-            json={**payment, "requestedAt": dt.isoformat()},
-        )
+    payload = {**payment, "requestedAt": requested_at.isoformat()}
+    resp = session.post(f"{Config.DEFAULT_PAYMENT_URL}/payments", json=payload)
 
-        if resp.status_code != 200:
-            raise Exception(f"Erro HTTP: {resp.status_code}")
+    resp.raise_for_status()
 
-        store_payment(payment, processor, dt.timestamp())
-    except Exception:
-        set_health(processor, False)
-        alt_processor = "fallback" if processor == "default" else "default"
-        if get_health(alt_processor):
-            pool.spawn(process_payment, payment)
-        else:
-            keydb.rpush("queue:payments", msgpack.packb(payment, use_bin_type=True))
+    store_payment(payment, "default", timestamp)
 
 
-def handle_sigint(signum, frame):
-    global running
-    print("[SIGNAL] SIGINT received, terminating...")
-    running = False
+def worker():
+    session = get_sesstion()
 
-
-def handle_sigterm(signum, frame):
-    global running
-    print("[SIGNAL] SIGTERM received, terminating...")
-    running = False
-
-
-def worker_loop():
-    print("[Worker] Starting main loop...")
-    while running:
+    while True:
         try:
-            result = keydb.blpop("queue:payments", timeout=2)
-            if result:
-                _, payment_data = result
+            data = redis.brpop("queue:payments", timeout=1)
+            if data:
+                _, payment_data = data
                 payment = msgpack.unpackb(payment_data, raw=False)
-                pool.spawn(process_payment, payment)
-        except Exception as e:
-            print(f"[ERROR] Loop of worker: {e}")
-            gevent.sleep(1)
-
-    print("[Worker] Closing, waiting for tasks to finish...")
-    pool.join()
+                process_payment(payment, session)
+        except requests.HTTPError:
+            traceback.print_exc()
+        except Exception:
+            pass
 
 
 if __name__ == "__main__":
-    signal.signal(signal.SIGINT, handle_sigint)
-    signal.signal(signal.SIGTERM, handle_sigterm)
-
-    try:
-        create_sessions()
-        worker_loop()
-    except KeyboardInterrupt:
-        pass
+    print("[Worker] Starting...")
+    gevent.joinall(
+        [gevent.spawn(worker) for _ in range(Config.NUM_WORKERS)],
+    )
---
+++ worker/confg.py
@@ -0,0 +1,20 @@
+import os
+from collections.abc import Callable
+from typing import Any, TypeVar
+
+T = TypeVar("T")
+
+
+def env(key: str, default: Any = None, parse: Callable[[str], T] = str) -> T:
+    return parse(os.getenv(key, default))
+
+
+class Config:
+    DEFAULT_PAYMENT_URL = env("DEFAULT_PAYMENT_URL", "http://localhost:8001")
+    FALLBACK_PAYMENT_URL = env("FALLBACK_PAYMENT_URL", "http://localhost:8002")
+
+    KEYDB_URL = env("KEYDB_URL", "redis://localhost")
+
+    NUM_WORKERS = env("NUM_WORKERS", "30", int)
+    BACKOFF_FACTOR = env("BACKOFF_FACTOR", "0.3", float)
+    NUM_OF_RETRY = env("NUM_OF_RETRY", "6", int)
