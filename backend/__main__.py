1d1115a7a5bf545c02f72efb8d1b5dbfd6f9ba50
Lucas Santana <lsfratel@gmail.com>
2025-08-02 17:28:56 -0300

    feat: unix socket

--- backend/__main__.py
+++ backend/__main__.py
@@ -1,7 +1,17 @@
+import os
+
 import bjoern
 
 from .wsgi import create_app
 
+socket = os.getenv("UNIX_SOCKET", None)
+
 if __name__ == "__main__":
-    print("[Server] Listening on port 9999...")
-    bjoern.run(create_app(), "0.0.0.0", 9999)
+    if socket:
+        if os.path.exists(socket):
+            os.remove(socket)
+        print(f"[Server] Listening on unix:{socket}.")
+        bjoern.run(create_app(), f"unix:{socket}")
+    else:
+        print("[Server] Listening on port 9999.")
+        bjoern.run(create_app(), "0.0.0.0", 9999)
--- worker/__main__.py
+++ worker/__main__.py
@@ -14,7 +14,6 @@ redis = redis.StrictRedis(
     connection_pool=redis.ConnectionPool.from_url(
         Config.KEYDB_URL,
         max_connections=Config.NUM_WORKERS,
-        decode_responses=False,
     ),
 )
 
@@ -22,7 +21,7 @@ redis = redis.StrictRedis(
 def get_sesstion():
     adapter = HTTPAdapter(
         pool_connections=Config.NUM_WORKERS,
-        pool_maxsize=Config.NUM_WORKERS * 2,
+        pool_maxsize=Config.NUM_WORKERS,
         max_retries=Retry(
             total=Config.NUM_OF_RETRY,
             backoff_factor=Config.BACKOFF_FACTOR,
@@ -30,18 +29,15 @@ def get_sesstion():
             status_forcelist={500},
         ),
     )
-
     session = requests.Session()
     session.mount("https://", adapter)
     session.mount("http://", adapter)
-
     return session
 
 
 def store_payment(payment: dict, processor: str, timestamp: float):
     key = f"payment:{processor}:{payment['correlationId']}"
-    pipe = redis.pipeline()
-
+    pipe = redis.pipeline(transaction=False)
     pipe.set(
         key,
         msgpack.packb(
@@ -50,40 +46,42 @@ def store_payment(payment: dict, processor: str, timestamp: float):
         ),
     )
     pipe.zadd("payments", {key: timestamp})
-
     pipe.execute()
 
 
 def process_payment(payment: dict, session: requests.Session):
     requested_at = datetime.now(timezone.utc)
     timestamp = requested_at.timestamp()
-
-    payload = {**payment, "requestedAt": requested_at.isoformat()}
-    resp = session.post(f"{Config.DEFAULT_PAYMENT_URL}/payments", json=payload)
-
+    payload = {
+        **payment,
+        "requestedAt": requested_at.isoformat(),
+    }
+    resp = session.post(
+        f"{Config.DEFAULT_PAYMENT_URL}/payments",
+        json=payload,
+    )
     resp.raise_for_status()
-
     store_payment(payment, "default", timestamp)
 
 
 def worker():
     session = get_sesstion()
-
     while True:
         try:
-            data = redis.brpop("queue:payments", timeout=1)
-            if data:
-                _, payment_data = data
-                payment = msgpack.unpackb(payment_data, raw=False)
-                process_payment(payment, session)
+            _, payment_data = redis.brpop("queue:payments", timeout=0)
+            payment = msgpack.unpackb(payment_data, raw=False)
+            process_payment(payment, session)
         except requests.HTTPError:
+            print("[Worker] Unexpected HTTPError:")
             traceback.print_exc()
         except Exception:
-            pass
+            print("[Worker] Unexpected error:")
+            traceback.print_exc()
+        gevent.sleep(0.05)
 
 
 if __name__ == "__main__":
-    print("[Worker] Starting...")
+    print("[Worker] Starting workers...")
     gevent.joinall(
         [gevent.spawn(worker) for _ in range(Config.NUM_WORKERS)],
     )
